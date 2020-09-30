e1061b6907a12010e609c6b53678e5de778ec0a8
Abhishek Doifode <45434232+abhishekdoifode1@users.noreply.github.com>
2020-09-30 15:45:43 +0530

    Update ReversingList.py

--- ReversingList.py
+++ ReversingList.py
@@ -3,5 +3,5 @@ def Reverse(lst):
 	return [ele for ele in reversed(lst)] 
 	
 # Just add following gto the list 10, 11, 12, 13, 14, 15
-lst = [] 
+lst = [ 10, 11, 12, 13, 14, 15] 
 print(Reverse(lst)) 
