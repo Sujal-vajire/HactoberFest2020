1a96d07e22b89e75864692fc1b0d683ddf10e440
abhi <abhinavverma698@gmail.com>
2020-10-25 16:39:37 +0530

    Hackerrank Problem Solution By Abhinav Verma

---
+++ Coin_Change_Problem.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'getWays' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. LONG_INTEGER_ARRAY c
+ */
+
+long getWays(int n, vector<long> S) {
+    sort(S.begin(),S.end());
+    long m = S.size();
+    long table[n+1][m];
+    memset(table,0,sizeof table);
+    long i,j,x,y;
+    for (i = 0; i < m; i++) 
+        table[0][i] = 1; 
+    for (i = 1; i < n + 1; i++) 
+    { 
+        for (j = 0; j < m; j++) 
+        { 
+            // Count of solutions including S[j] 
+            x = (i-S[j] >= 0) ? table[i - S[j]][j] : 0; 
+  
+            // Count of solutions excluding S[j] 
+            y = (j >= 1) ? table[i][j - 1] : 0; 
+  
+            // total count 
+            table[i][j] = x + y; 
+        } 
+    } 
+    return table[n][m - 1]; 
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int m = stoi(first_multiple_input[1]);
+
+    string c_temp_temp;
+    getline(cin, c_temp_temp);
+
+    vector<string> c_temp = split(rtrim(c_temp_temp));
+
+    vector<long> c(m);
+
+    for (int i = 0; i < m; i++) {
+        long c_item = stol(c_temp[i]);
+
+        c[i] = c_item;
+    }
+
+    // Print the number of ways of making change for 'n' units using coins having the values given by 'c'
+
+    long ways = getWays(n, c);
+
+    fout << ways << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
